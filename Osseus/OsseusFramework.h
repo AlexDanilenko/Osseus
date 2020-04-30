e3633d54f06ff2865a78afb0590cdf78338cd1ca
Oleksandr Danylenko <>
2020-05-01 00:19:56 +0300

    merged with test/add-proj

---
+++ Osseus/OsseusFramework.h
@@ -0,0 +1,19 @@
+//
+//  Osseus.h
+//  Osseus
+//
+//  Created by Oleksandr Danylenko on 01.05.2020.
+//  Copyright © 2020 Oleksandr Danylenko. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Osseus.
+FOUNDATION_EXPORT double OsseusVersionNumber;
+
+//! Project version string for Osseus.
+FOUNDATION_EXPORT const unsigned char OsseusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OsseusFramework/PublicHeader.h>
+
+
